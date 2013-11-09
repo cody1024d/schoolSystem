f2e8628501ab79eb79423d1a4d8919838b38b310
Cody <cody1024d@gmail.com>
2013-11-09 10:11:50 -0600

    Adding the School User model hierarchy -- Still needs authentication logic

---
+++ db/migrate/20131109160056_create_school_users.rb
@@ -0,0 +1,9 @@
+class CreateSchoolUsers < ActiveRecord::Migration
+  def change
+    create_table :school_users do |t|
+      t.string :email
+      t.string :type
+      t.timestamps
+    end
+  end
+end
